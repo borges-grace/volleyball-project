09a1034ac032864f6d76ae8d781cbec67c8affd2
borges-grace <borges-grace@users.noreply.github.com>
2022-01-30 20:22:20 -0700

    Adding vue, vuetify, vuex, and vue router to project

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-card>
+      <v-card-title> <v-row class="text-center">Welcome! </v-row></v-card-title>
+      <v-card-text>
+        <v-row>
+          <v-col cols="auto">
+            <v-text-field label="Enter your team name" />
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
