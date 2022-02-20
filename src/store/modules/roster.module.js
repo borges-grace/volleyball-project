5024217ce9e9dba7645bfae4e5d76a5ef496aaf1
borges-grace <gracelehuanani@gmail.com>
2022-02-20 13:31:34 -0700

    getting rid of error

---
+++ src/store/modules/roster.module.js
@@ -0,0 +1,22 @@
+
+
+const state = {
+    roster: { isLoading: false, data: new Map(), errMsg: null}
+};
+
+const actions = {};
+
+const mutations = {};
+
+// const helpers = {};
+
+const getters = {};
+
+export default {
+    name: "roster",
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
+}
