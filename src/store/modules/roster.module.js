74210a354be604eb79ee4a5af094e9579fe28b56
borges-grace <gracelehuanani@gmail.com>
2022-02-22 19:49:06 -0700

    created post request for roster module

--- src/store/modules/roster.module.js
+++ src/store/modules/roster.module.js
@@ -1,22 +1,48 @@
-
+// export the mutations
+export let [POST_ROSTER] = ["putRoster"];
 
 const state = {
-    roster: { isLoading: false, data: new Map(), errMsg: null}
+  roster: { isLoading: false, data: new Map(), errMsg: null },
 };
 
-const actions = {};
+const actions = {
+  postRoster: ({ commit }, roster) => {
+    // will implement api calls when I build the back end
+    commit(POST_ROSTER, roster);
+  },
+};
 
-const mutations = {};
+const mutations = {
+  [POST_ROSTER]: (state, roster) => {
+    state.roster.data = new Map(
+      roster.map((athlete) => [athlete.JerseyNumber, athlete])
+    );
+  },
+};
 
-// const helpers = {};
+const helpers = {
+  sortByLastName(a, b) {
+    if (a.LastName.toLowerCase() < b.LastName.toLowerCase()) {
+      return -1;
+    } else if (a.LastName.toLowerCase() > b.LastName.toLowerCase()) {
+      return 1;
+    } else {
+      return 0;
+    }
+  },
+};
 
-const getters = {};
+const getters = {
+  getRoster: (state) => {
+    return Array.from(state.roster.data.values()).sort(helpers.sortByLastName);
+  },
+};
 
 export default {
-    name: "roster",
-    namespaced: true,
-    state,
-    getters,
-    actions,
-    mutations
-}
+  name: "roster",
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
